4c0275dc555a251ed9775aa15a8bc09d7ec583fe
virelai <szwarnogjustyna@gmail.com>
2016-03-15 17:50:04 +0000

    webpack dev server correct configuration

---
+++ app/index.js
@@ -0,0 +1,7 @@
+import angular from 'angular';
+
+var ngModule = angular.module('app', []);
+
+//import './style.css'
+import './ng-app-start.js'
+console.log(ngModule);
---
+++ app/ng-app-start.js
@@ -0,0 +1,7 @@
+// angular.module('app', [
+
+// ])
+// .controller('HomeController', function($scope){
+//     $scope.myValue = '50 PLN';
+// })
+alert("Hello");
