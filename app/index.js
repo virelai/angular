4fe5ef893fee268ea769ab77103e4fea4dde8ba1
virelai <szwarnogjustyna@gmail.com>
2016-03-22 22:32:16 +0000

    angular app with modules

--- app/index.js
+++ app/index.js
@@ -1,11 +1,12 @@
 import 'bootstrap/dist/css/bootstrap.css';
+
 import angular from 'angular';
 import uirouter from 'angular-ui-router';
-import routing from './config';
-//import './style.css'
 
-//console.log(uirouter);
-var ngModule = 
-                angular
-                        .module('app', [uirouter])
-                        .config(routing);
+import routing from './config';
+
+import home from './features/home';
+
+const ngModule = angular
+                    .module('app', [uirouter, home])
+                    .config(routing);
