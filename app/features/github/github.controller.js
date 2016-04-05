7a79e60275b273366acafbda248995ee83d77664
virelai <szwarnogjustyna@gmail.com>
2016-04-05 17:14:18 +0000

    gihub repos list get and show in view

--- app/features/github/github.controller.js
+++ app/features/github/github.controller.js
@@ -3,13 +3,13 @@ export default class GitHubController {
     constructor($http, $scope)
     {
         this.$http = $http;
-        this.email;
+        this.reposArray;
         this.makeRequest();
     }
         
         makeRequest(){
             
-            this.$http.get('https://api.github.com/users/bslipek')
+            this.$http.get('https://api.github.com/users/bslipek/repos')
             .then((response)=>{
                 this.assignToScope(response)
             });
@@ -19,8 +19,17 @@ export default class GitHubController {
         
         assignToScope(response){
             
-            console.log(response.data.email);
-            this.email = response.data.email;
+            this.reposArray = response.data;
+            
+            
+            for (var i = 0; i < this.reposArray.length; i++) {
+                
+                //this.reposArray = 
+                console.log(this.reposArray[i].name);
+            }
+            
+            
+            //this.email = response.data.email;
         }
 }
         
