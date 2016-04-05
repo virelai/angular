ecb1114180b4e1d6867e29a640bf540f97197d60
virelai <szwarnogjustyna@gmail.com>
2016-04-05 16:56:36 +0000

    github get email and pass to view

--- app/features/github/github.controller.js
+++ app/features/github/github.controller.js
@@ -1,3 +1,26 @@
 export default class GitHubController {
-
+    
+    constructor($http, $scope)
+    {
+        this.$http = $http;
+        this.email;
+        this.makeRequest();
+    }
+        
+        makeRequest(){
+            
+            this.$http.get('https://api.github.com/users/bslipek')
+            .then((response)=>{
+                this.assignToScope(response)
+            });
+            
+        }
+        
+        
+        assignToScope(response){
+            
+            console.log(response.data.email);
+            this.email = response.data.email;
+        }
 }
+        
